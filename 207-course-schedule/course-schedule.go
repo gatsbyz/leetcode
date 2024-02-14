e3befb4a7b27811e3b470591e7dca9779859f76a
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-02-13 20:43:25 -0500

    Time: 9 ms (70.39%) | Memory: 5.2 MB (85.26%) - LeetSync

--- 207-course-schedule/course-schedule.go
+++ 207-course-schedule/course-schedule.go
@@ -1,43 +1,54 @@
 func canFinish(numCourses int, prerequisites [][]int) bool {
-    // Create a graph from the prerequisites
-    graph := make([][]int, numCourses)
-    for _, pre := range prerequisites {
-        graph[pre[1]] = append(graph[pre[1]], pre[0])
+    // Build the graph using an adjacency list.
+    // graph[i] contains a list of courses that course i depends on.
+    adjList := make([][]int, numCourses)
+    for _, prereq := range prerequisites {
+        adjList[prereq[0]] = append(adjList[prereq[0]], prereq[1])
     }
 
-    // visited array to keep track of visited nodes during DFS
-    // state: 0 = unvisited, 1 = visiting, 2 = visited
+    // visited[i] == 0 means course i has not been visited,
+    // visited[i] == 1 means course i is being visited (in the current DFS path),
+    // visited[i] == 2 means course i has been fully visited (all descendants explored).
     visited := make([]int, numCourses)
-    
+
+    // Define the DFS function to explore the graph.
     var dfs func(int) bool
     dfs = func(course int) bool {
-        if visited[course] == 1 { // cycle detected
+        if visited[course] == 1 {
+            // We are visiting a course that is already being visited,
+            // which indicates a cycle.
             return false
         }
-        if visited[course] == 2 { // already visited
+        if visited[course] == 2 {
+            // This course has been fully explored previously,
+            // no need to explore again. No cycle detected from this course.
             return true
         }
 
-        // Mark the current node as visiting
+        // Mark the course as being visited.
         visited[course] = 1
 
-        // Visit all the adjacent nodes
-        for _, nextCourse := range graph[course] {
-            if !dfs(nextCourse) {
+        // Visit all the prerequisites of the current course.
+        for _, prereq := range adjList[course] {
+            if !dfs(prereq) {
+                // If a cycle is detected in any prerequisite, return false.
                 return false
             }
         }
 
-        // Mark the current node as visited
+        // Mark the course as fully visited.
         visited[course] = 2
         return true
     }
 
-    // Check if the graph has a cycle
+    // Perform DFS from each course to detect cycles.
     for i := 0; i < numCourses; i++ {
         if !dfs(i) {
+            // If a cycle is detected from any course, return false.
             return false
         }
     }
+
+    // If no cycles are detected, it is possible to finish all courses.
     return true
 }
