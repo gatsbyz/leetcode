1bb293e58ebea2f312c1c75314fddb86c34abd2c
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-02-13 19:50:50 -0500

    Time: 9 ms (70.39%) | Memory: 5.1 MB (85.26%) - LeetSync

--- 207-course-schedule/course-schedule.go
+++ 207-course-schedule/course-schedule.go
@@ -1,38 +1,43 @@
 func canFinish(numCourses int, prerequisites [][]int) bool {
+    // Create a graph from the prerequisites
     graph := make([][]int, numCourses)
+    for _, pre := range prerequisites {
+        graph[pre[1]] = append(graph[pre[1]], pre[0])
+    }
 
-	//construct graph
-	for i := 0; i < len(prerequisites); i++ {
-		graph[prerequisites[i][1]] = append(graph[prerequisites[i][1]], prerequisites[i][0])
-	}
-	visited := make([]bool, len(graph))
-	curNodeOnPath := make([]bool, len(graph))
-	
-	//check all the path starting from different nodes
-	for i := 0; i < len(graph); i++ { 
-        if !dfs(graph, i, visited, curNodeOnPath) {
+    // visited array to keep track of visited nodes during DFS
+    // state: 0 = unvisited, 1 = visiting, 2 = visited
+    visited := make([]int, numCourses)
+    
+    var dfs func(int) bool
+    dfs = func(course int) bool {
+        if visited[course] == 1 { // cycle detected
             return false
         }
-	}
-	return true
-}
-func dfs(graph [][]int, index int, visited, curNodeOnPath []bool) bool{
-    if visited[index] {
+        if visited[course] == 2 { // already visited
+            return true
+        }
+
+        // Mark the current node as visiting
+        visited[course] = 1
+
+        // Visit all the adjacent nodes
+        for _, nextCourse := range graph[course] {
+            if !dfs(nextCourse) {
+                return false
+            }
+        }
+
+        // Mark the current node as visited
+        visited[course] = 2
         return true
     }
-	 if curNodeOnPath[index] {
-        curNodeOnPath[index] = false
-        return false
-    }
-    curNodeOnPath[index] = true
-	for i := 0; i < len(graph[index]); i++ {
-        curNeighbor := graph[index][i]
-        if !dfs(graph, curNeighbor, visited, curNodeOnPath) {
+
+    // Check if the graph has a cycle
+    for i := 0; i < numCourses; i++ {
+        if !dfs(i) {
             return false
         }
-  
-	}
-    curNodeOnPath[index] = false
-    visited[index] = true
-	return true
-} 
+    }
+    return true
+}
