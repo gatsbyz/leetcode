7ffb2a8b394480b99b6bfdd552588da3ef249b75
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-02-16 10:18:10 -0500

    Time: 170 ms (5.26%) | Memory: 28.8 MB (5.26%) - LeetSync

---
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.go
@@ -0,0 +1,32 @@
+func findLeastNumOfUniqueInts(arr []int, k int) int {
+    recordMap := make(map[int]int)
+    records := make([][]int, 0)
+    for _, n := range arr {
+        recordMap[n]++
+    }
+    for n, f := range recordMap {
+        records = append(records, []int{n ,f})
+    }
+    slices.SortFunc(records, func(a, b []int) int {
+        return cmp.Compare(a[1], b[1])
+    })
+
+    fmt.Println(records)
+
+    count := 0
+    for _, record := range records {
+        if k > 0 {
+            if k > record[1] {
+                k -= record[1]
+                continue
+            }  
+            if k < record[1]{
+                count++
+            }
+            k = 0
+        } else {
+            count++
+        }
+    }
+    return count
+}
