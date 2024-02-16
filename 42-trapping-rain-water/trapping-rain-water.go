38af07e45e9e574452e69d81946b500b6472cb0b
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-02-16 09:39:37 -0500

    Time: 32 ms (9.54%) | Memory: 6.1 MB (26.18%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.go
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -1,30 +1,24 @@
 func trap(height []int) int {
-	left, right := 0, len(height)-1 // Two pointers
-	leftMax, rightMax := 0, 0       // Max heights from both sides
-	trapped := 0                    // Total trapped water
+    maxLeft, maxRight := 0, 0
+    left, right := 0, len(height)-1
+    trapped := 0
+    for left < right {
+        fmt.Println(trapped, left, right)
+        if height[left] > maxLeft {
+            maxLeft = height[left]
+        } 
+        if height[right] > maxRight {
+            maxRight = height[right]
+        }
 
-	// Iterate until the two pointers meet
-	for left < right {
-		// Update leftMax and rightMax
-		if height[left] > leftMax {
-			leftMax = height[left]
-		}
-		if height[right] > rightMax {
-			rightMax = height[right]
-		}
+        if maxLeft < maxRight {
+            trapped += maxLeft - height[left]
+            left++
+        } else {
+            trapped += maxRight - height[right]
+            right--
+        }
+    }
 
-		// Calculate trapped water based on the smaller of the two max heights
-		if leftMax < rightMax {
-			// Water trapped on the current left position is the difference
-			// between leftMax and the current height, if any
-			trapped += leftMax - height[left]
-			left++ // Move left pointer inwards
-		} else {
-			// Similarly, water trapped on the current right position
-			trapped += rightMax - height[right]
-			right-- // Move right pointer inwards
-		}
-	}
-
-	return trapped
+    return trapped
 }
