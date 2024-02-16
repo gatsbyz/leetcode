aa318bc072c7753892524c4b9983358cf2b42030
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-02-15 21:26:42 -0500

    Time: 12 ms (38.94%) | Memory: 6.3 MB (13.18%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -0,0 +1,32 @@
+func trap(height []int) int {
+    n := len(height)
+    res := 0
+    maxFromLeft := make([]int, n)
+    maxFromRight := make([]int, n)
+    for i := 1; i < n; i++ {
+        maxFromLeft[i] = max(maxFromLeft[i-1], height[i-1])
+    }
+    for j := n - 2; j >= 0; j-- {
+        maxFromRight[j] = max(maxFromRight[j+1], height[j+1])
+    }
+    for i := 1; i < n - 1; i++ {
+        if height[i] < maxFromLeft[i] && height[i] < maxFromRight[i] {
+            res += min(maxFromLeft[i], maxFromRight[i]) - height[i]
+        }
+    }
+    return res
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
