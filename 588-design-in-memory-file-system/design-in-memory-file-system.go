ebaedcd2c932fa1223e790320638a4548782bee7
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-02-17 20:53:01 -0500

    Time: 9 ms (83.05%) | Memory: 6.6 MB (83.05%) - LeetSync

--- 588-design-in-memory-file-system/design-in-memory-file-system.go
+++ 588-design-in-memory-file-system/design-in-memory-file-system.go
@@ -1,108 +1,62 @@
-type inode uint16
+type FileNode struct {
+    children map[string]*FileNode // Children could be both files and directories
+    content  string               // Non-empty for files
+    isFile   bool
+}
 
 type FileSystem struct {
-	root         *FileNode
-	inodeCounter inode
-	inodeDataMap map[inode][]byte
-}
-
-func (fs *FileSystem) incrementInode() {
-	fs.inodeCounter = fs.inodeCounter + 1
-}
-
-type FileNode struct {
-	name        string
-	isDirectory bool
-	inode       inode
-	children    map[string]*FileNode
-}
-
-func newFileNode(root *FileSystem, name string, isDirectory bool) *FileNode {
-	defer root.incrementInode()
-
-	return &FileNode{
-		name:        name,
-		isDirectory: isDirectory,
-		inode:       root.inodeCounter,
-		children:    make(map[string]*FileNode),
-	}
+    root *FileNode
 }
 
 func Constructor() FileSystem {
-    return FileSystem{
-        root: &FileNode{
-            name: "/",
-            isDirectory: true,
-            inode: 0,
-            children: make(map[string]*FileNode),
-        },
-        inodeCounter: 1,
-        inodeDataMap: make(map[inode][]byte),
-    }
+    return FileSystem{root: &FileNode{children: make(map[string]*FileNode)}}
 }
 
-// If path is a file path, returns a list that only contains this file's name.
-// If path is a directory path, returns the list of file and directory names in this directory.
-func (fs *FileSystem) Ls(path string) []string {
-    var allFiles []string
-    node := fs.root
-    for _, pathStr := range splitFilePath(path) {
-        node, _ = node.children[pathStr]
+func (this *FileSystem) Ls(path string) []string {
+    node := this.navigate(path)
+    if node.isFile {
+        // Extract file name from path
+        splitPath := strings.Split(path, "/")
+        return []string{splitPath[len(splitPath)-1]}
     }
-    if !node.isDirectory {
-        return []string{node.name}
-    } else {
-        for childNodeName := range node.children {
-            allFiles = append(allFiles, childNodeName)
+    // Collect and return directory contents
+    var contents []string
+    for name := range node.children {
+        contents = append(contents, name)
+    }
+    sort.Strings(contents) // Sort results lexicographically
+    return contents
+}
+
+func (this *FileSystem) Mkdir(path string) {
+    this.navigate(path) // Simply navigate to the path, nodes will be created if not exist
+}
+
+func (this *FileSystem) AddContentToFile(filePath string, content string) {
+    node := this.navigate(filePath)
+    node.content += content // Append content
+    node.isFile = true      // Mark as file
+}
+
+func (this *FileSystem) ReadContentFromFile(filePath string) string {
+    node := this.navigate(filePath)
+    return node.content
+}
+
+// Helper function to navigate through the file system and optionally create nodes
+func (this *FileSystem) navigate(path string) *FileNode {
+    current := this.root
+    if path == "/" {
+        return current
+    }
+    parts := strings.Split(path, "/")[1:] // Ignore the first empty part due to leading '/'
+    for _, part := range parts {
+        if _, exists := current.children[part]; !exists {
+            current.children[part] = &FileNode{children: make(map[string]*FileNode)}
         }
+        current = current.children[part]
     }
-    sort.Strings(allFiles)
-    return allFiles
-}
-
-// Makes a new directory according to the given path. The given directory path does not exist. If the middle directories in the path do not exist, you should create them as well.
-func (fs *FileSystem) Mkdir(path string)  {
-    createFileIfNotExists(path, fs, true)
-}
-
-// If filePath does not exist, creates that file containing given content.
-// If filePath already exists, appends the given content to original content.
-func (fs *FileSystem) AddContentToFile(filePath string, content string)  {
-    node := createFileIfNotExists(filePath, fs, false)
-    fs.inodeDataMap[node.inode] = append(fs.inodeDataMap[node.inode], []byte(content)...)
-}
-
-// Returns the content in the file at filePath.
-func (fs *FileSystem) ReadContentFromFile(filePath string) string {
-    node := createFileIfNotExists(filePath, fs, false)
-    return string(fs.inodeDataMap[node.inode])
-}
-
-func splitFilePath(path string) []string {
-	if path == "/" {
-		return []string{}
-	}
-	split := strings.Split(path, "/")
-	return split[1:]
-}
-
-func createFileIfNotExists(path string, root *FileSystem, isDirectory bool) *FileNode {
-	node := root.root
-	for _, pathStr := range splitFilePath(path) {
-		//if child is not found, then create a new file node
-		if childNode, ok := node.children[pathStr]; !ok {
-			//we need to create the file node here
-			fileNode := newFileNode(root, pathStr, isDirectory)
-			//tag this fileNode to the filesystem
-			node.children[pathStr] = fileNode
-			//the newly created node is used to recur
-			node = fileNode
-		} else {
-			//recur for the already created child
-			node = childNode
-		}
-	}
-	return node
+    return current
 }
 
 
