02f2755e01cd697d845a954ca0540af725aeb71a
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-02-18 12:19:36 -0500

    Time: 13 ms (49.15%) | Memory: 6.4 MB (93.22%) - LeetSync

--- 588-design-in-memory-file-system/design-in-memory-file-system.go
+++ 588-design-in-memory-file-system/design-in-memory-file-system.go
@@ -15,7 +15,7 @@ func Constructor() FileSystem {
 
 
 func (fs *FileSystem) Ls(path string) []string {
-    file := fs.discover(path)
+    file := fs.discover(path, false, false)
     if file == nil {
         return []string{}
     }
@@ -33,12 +33,12 @@ func (fs *FileSystem) Ls(path string) []string {
 
 
 func (fs *FileSystem) Mkdir(path string)  {
-    fs.discover(path)
+    fs.discover(path, false, true)
 }
 
 
 func (fs *FileSystem) AddContentToFile(filePath string, content string)  {
-    file := fs.discover(filePath)
+    file := fs.discover(filePath, true, true)
     // add conditional - when returned is a directory
     file.isDirectory = false 
     file.contents += content
@@ -46,25 +46,32 @@ func (fs *FileSystem) AddContentToFile(filePath string, content string)  {
 
 
 func (fs *FileSystem) ReadContentFromFile(filePath string) string {
-    file := fs.discover(filePath)
+    file := fs.discover(filePath, true, false)
     if file.isDirectory {
         // panic
     }
     return file.contents
 }
 
-func (fs *FileSystem) discover(filePath string) *File {
+func (fs *FileSystem) discover(filePath string, isFile, isCreate bool) *File {
     filePathSplits := strings.Split(filePath, "/")
 
     curr := fs.root
-    for _, path := range filePathSplits {
+    for index, path := range filePathSplits {
         if path == "" {
             continue
         }
         if _, exists := curr.children[path] ; !exists {
-            curr.children[path] = &File{children: make(map[string]*File), isDirectory: true}
+            if isCreate {
+                curr.children[path] = &File{children: make(map[string]*File), isDirectory: true}
+            } else {
+                return nil
+            }
         }
         curr = curr.children[path]
+        if index == len(filePathSplits)-1 && isFile {
+            curr.isDirectory = false
+        }
     }
     return curr
 }
