4a3214e20886e427e166e5b4b5aec50839969ec6
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-02-20 10:03:54 -0500

    Time: 238 ms (5.76%) | Memory: 12.9 MB (21.65%) - LeetSync

---
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.go
@@ -0,0 +1,19 @@
+func divideArray(nums []int, k int) [][]int {
+    sort.SliceStable(nums, func(i, j int) bool {
+        return nums[i] < nums[j]
+    })
+    array := [][]int{}
+    for index, num := range nums {
+        // 0-0,1,2 1-3,4,5
+        arrayIndex := index / 3
+        subIndex := index % 3
+        if subIndex == 0 {
+            array = append(array, make([]int, 0))
+        }
+        array[arrayIndex] = append(array[arrayIndex], num)
+        if (subIndex == 2 || index == len(nums)-1) && array[arrayIndex][len(array[arrayIndex])-1]-array[arrayIndex][0] > k {
+            return [][]int{}
+        }
+    }
+    return array
+}
