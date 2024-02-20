2054d5d0175c9fee23658a514635f4aa24e17efc
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-02-20 18:38:17 -0500

    Time: 11 ms (43.03%) | Memory: 2.6 MB (92.51%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -1,25 +1,21 @@
 func lengthOfLongestSubstring(s string) int {
-    stack := []string{}
+    stack := [200]bool{}
     maxCount := 0
-    countStr := map[string]int{}
+    length := 0
+    pointer := 0
     for index:=0;index<len(s);index++ {
-        fmt.Println(countStr[string(s[index])])
-        if len(stack) != 0 && countStr[string(s[index])] > 0 {
-            for i:=0;i<len(stack);i++ {
-                if stack[i] == string(s[index]) {
-                    stack = stack[i+1:]
-                    countStr[string(s[index])]=0
-                    break
-                }
-                countStr[string(s[index])]=0
+        fmt.Println(s[index])
+        if stack[s[index]] {
+            for ; stack[s[index]] ; pointer++ {
+                stack[s[pointer]] = false
+                length--
             }
         }
-        stack = append(stack, string(s[index]))
-        countStr[string(s[index])]++
-        if maxCount < len(stack) {
-            maxCount = len(stack)
+        stack[s[index]] = true
+        length++
+        if maxCount < length {
+            maxCount = length
         }
-        fmt.Println(len(stack), stack, maxCount, countStr[string(s[index])])
     }
     return maxCount
 }
