b45b09bb4a0a6e01c0e270006206ba650ecddad5
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-03-01 09:05:52 -0500

    Time: 3 ms (93.49%) | Memory: 4.2 MB (19.78%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,21 +1,33 @@
 func twoSum(nums []int, target int) []int {
-    start, end := 0, len(nums)-1
-    save := append([]int{}, nums...)
-    sort.Ints(save)
-    for start < end {
-        if save[start] + save[end] == target {
-            break
-        } else if save[start] + save[end] < target {
-            start++
-        } else {
-            end--
+    calc := map[int]int{} // key num, value index
+
+    for i, num := range nums {
+        find := target - num
+        if _, exists := calc[find]; exists {
+            return []int{calc[find], i}
         }
+        calc[num] = i
     }
-    res := []int{}
-    for i, val := range nums {
-        if val == save[start] || val == save[end] {
-            res = append(res, i)
-        }
-    }
-    return res
+
+    return []int{}
+
+    // start, end := 0, len(nums)-1
+    // save := append([]int{}, nums...)
+    // sort.Ints(save)
+    // for start < end {
+    //     if save[start] + save[end] == target {
+    //         break
+    //     } else if save[start] + save[end] < target {
+    //         start++
+    //     } else {
+    //         end--
+    //     }
+    // }
+    // res := []int{}
+    // for i, val := range nums {
+    //     if val == save[start] || val == save[end] {
+    //         res = append(res, i)
+    //     }
+    // }
+    // return res
 }
