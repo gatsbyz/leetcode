648f03ac77be7f866732537fdddb01400fa63500
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-03-07 17:22:28 -0500

    Time: 12 ms (21.64%) | Memory: 2.7 MB (62.19%) - LeetSync

--- 69-sqrtx/sqrtx.go
+++ 69-sqrtx/sqrtx.go
@@ -1,14 +1,35 @@
+
+import "fmt"
 func mySqrt(x int) int {
-    i := 1
-    for i <= x {
-        if i*i == x {
-            return i
-        } else if i*i > x {
-            return i-1
-        }
-        i++
+    l, r := 0, x
+
+    if x == 0 || x == 1 {
+        return x
     }
-    return 0
+
+    for l<r {
+        mid := (l+r)/2
+        if mid*mid == x {
+            return mid
+        } else if mid*mid < x {
+            l = mid+1
+        } else {
+            r = mid
+        }
+        fmt.Println(l, r,  mid)
+    }
+    return l-1
+
+    // i := 1
+    // for i <= x {
+    //     if i*i == x {
+    //         return i
+    //     } else if i*i > x {
+    //         return i-1
+    //     }
+    //     i++
+    // }
+    // return 0
 }
 
 // 4 -> 2
