63a6e8000766669c793667623a582e6f93561079
Jesse Lee <gatsbyz@users.noreply.github.com>
2024-03-25 12:36:19 -0400

    Time: 48 ms (51.56%) | Memory: 7.7 MB (35.16%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.go
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.go
@@ -1,8 +1,5 @@
 func findDuplicates(nums []int) []int {
     dupes := []int{}
-    if len(nums) == 1 {
-        return dupes
-    }
     for _, num := range nums {
         if (nums[(num-1)%len(nums)]-1) / len(nums) == 1 {
             dupes = append(dupes, ((num-1) % len(nums)) + 1)
